ccbb35c8c99712fda4c0e016c0712457bbf65d76
JoaoGuimaraes22 <JoaoGuimaraes22@users.noreply.github.com>
2021-11-30 10:23:06 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.32dfab69edc90e8b79d27c4b0c69a087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4597f0acf60bb9aca8a22ff04e7f89be",
+    "url": "/ActionsTest/index.html"
+  },
+  {
+    "revision": "3bfe2b34db67b5cd20da",
+    "url": "/ActionsTest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de9bfdff00174e6ab9f0",
+    "url": "/ActionsTest/static/js/2.f712c785.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ActionsTest/static/js/2.f712c785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfe2b34db67b5cd20da",
+    "url": "/ActionsTest/static/js/main.e6fcd14d.chunk.js"
+  },
+  {
+    "revision": "62458482ffda3a9dce8c",
+    "url": "/ActionsTest/static/js/runtime-main.2005ce2a.js"
+  }
+]);
